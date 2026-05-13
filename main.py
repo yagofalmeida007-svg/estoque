b2fb1110f84b8e35dd8d1f37aaebc00b7aeb4595
yagofalmeida007-svg <yagofalmeida007@gmail.com>
2026-05-13 19:30:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+# MENU
+while True:
+
+    print("\n===== SISTEMA DE ESTOQUE =====")
+    print("1 - Cadastrar produto")
+    print("2 - Registrar entrada")
+    print("3 - Registrar saída")
+    print("4 - Consultar estoque")
+    print("5 - Alertar estoque baixo")
+    print("6 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+
+        nome = input("Nome do produto: ")
+        categoria = input("Categoria: ")
+        preco = float(input("Preço: R$ "))
+        quantidade = int(input("Quantidade: "))
+
+        cadastrar_produto(nome, categoria, preco, quantidade)
+
+    elif opcao == "2":
+
+        produto = input("Produto: ")
+        quantidade = int(input("Quantidade de entrada: "))
+
+        registrar_entrada(produto, quantidade)
+
+    elif opcao == "3":
+
+        produto = input("Produto: ")
+        quantidade = int(input("Quantidade de saída: "))
+
+        registrar_saida(produto, quantidade)
+
+    elif opcao == "4":
+
+        produto = input("Produto: ")
+
+        consultar_estoque(produto)
+
+    elif opcao == "5":
+
+        limite = int(input("Limite mínimo: "))
+
+        alertar_estoque_baixo(limite)
+
+    elif opcao == "6":
+
+        print("Programa encerrado.")
+        break
+
+    else:
+        print("Opção inválida!")
